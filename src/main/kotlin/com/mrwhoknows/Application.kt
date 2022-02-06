cc4ff7cfc147364e7b6abfe29169abf9b2121d02
Avadhut Tanugade <avdhuttanugade55@gmail.com>
2022-02-06 13:34:11 +0530

    Added Heroku Config

--- src/main/kotlin/com/mrwhoknows/Application.kt
+++ src/main/kotlin/com/mrwhoknows/Application.kt
@@ -1,5 +1,7 @@
 package com.mrwhoknows
 
+import com.mrwhoknows.config.Constants.DB_URL
+import com.mrwhoknows.config.Constants.PORT
 import com.mrwhoknows.data.model.ShortUrl
 import com.mrwhoknows.data.repo.ShortUrlRepo
 import com.mrwhoknows.data.repo.ShortUrlRepoImpl
@@ -10,11 +12,8 @@ import org.litote.kmongo.coroutine.coroutine
 import org.litote.kmongo.reactivestreams.KMongo
 
 fun main() {
-    // TODO: create constants and env vars
-    val shortUrlConnection = KMongo.createClient().coroutine.getDatabase("short-url").getCollection<ShortUrl>()
+    val shortUrlConnection = KMongo.createClient(DB_URL).coroutine.getDatabase("short-url").getCollection<ShortUrl>()
     val repository: ShortUrlRepo = ShortUrlRepoImpl(shortUrlConnection)
-    val port = System.getenv("PORT").toInt()
-    val PORT = if (port < 0) 80 else port
     embeddedServer(Netty, port = PORT, host = "0.0.0.0") {
         // TODO: introduce DI
         configureRoutes(repository)
